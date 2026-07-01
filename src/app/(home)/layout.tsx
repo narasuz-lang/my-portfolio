b99475feaa368e31a696ce0155bf2a2be01c8e0d
narasuz-lang <nareshram4804@gmail.com>
2026-07-01 16:42:54 +0530

    Force home page redirect

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,6 +1,6 @@
-import { HomeLayout } from 'fumadocs-ui/layouts/home';
-import { baseOptions } from '@/lib/layout.shared';
+// src/app/page.tsx
+import { redirect } from 'next/navigation';
 
-export default function Layout({ children }: LayoutProps<'/'>) {
-  return <HomeLayout {...baseOptions()}>{children}</HomeLayout>;
+export default function RootPage() {
+  redirect('/docs');
 }
